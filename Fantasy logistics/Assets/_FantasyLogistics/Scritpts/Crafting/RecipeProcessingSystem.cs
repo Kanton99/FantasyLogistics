b86f2ec4fba4e3e07a1df6fb819fdb5a5018abf3
Kanton99 <avolkov99@gmail.com>
2025-05-19 14:22:10 +0200

    Recipe input usage

--- Fantasy logistics/Assets/_FantasyLogistics/Scritpts/Crafting/RecipeProcessingSystem.cs
+++ Fantasy logistics/Assets/_FantasyLogistics/Scritpts/Crafting/RecipeProcessingSystem.cs
@@ -12,6 +12,7 @@ namespace FantasyLogistics
 		{
 			var deltaTime = SystemAPI.Time.DeltaTime;
 			var outputLookup = SystemAPI.GetComponentLookup<ItemComponent>(isReadOnly: false);
+			var inputLookup = SystemAPI.GetComponentLookup<ItemComponent>(isReadOnly: false);
 
 			foreach (var (recipeData, recipeState, recipeInputs, recipeOutput) in SystemAPI.Query<RefRO<RecipeData>, RefRW<RecipeState>, RefRO<RecipeInputs>, RefRW<RecipeOutput>>())
 			{
@@ -37,10 +38,31 @@ namespace FantasyLogistics
 				}
 				else
 				{
-					recipeState.ValueRW.isProcessing = true;
-					recipeState.ValueRW.timeRemaining = recipeBlob.Value.craftingTime;
 					//TODO consume inputs
+					bool canStart = true;
+					int count = 0;
+					foreach (var item in recipeInputs.ValueRO.Value)
+					{
+						var inputItem = inputLookup[item];
+						if (inputItem.amount < recipeBlob.Value.inputs[count].amount)
+							canStart = false;
+						count++;
+					}
 
+					if (canStart)
+					{
+						count = 0;
+						foreach (var item in recipeInputs.ValueRO.Value)
+						{
+							var inputItem = inputLookup[item];
+							inputItem.amount -= recipeBlob.Value.inputs[count].amount;
+							inputLookup[item] = inputItem;
+							count++;
+						}
+						recipeState.ValueRW.isProcessing = true;
+						recipeState.ValueRW.timeRemaining = recipeBlob.Value.craftingTime;
+					}
+					// else Debug.Log("Can't start recipe, not enough resources");
 				}
 			}
 		}
